2a5609c76d960330dd816f1a3a14c2d2db5d5355
NANDO <amnando.bs@gmail.com>
2025-12-30 12:54:41 +0330

    relearn test writing and reabuild test file with 4 simple test

--- api/tests.py
+++ api/tests.py
@@ -73,68 +73,101 @@
 #         print(response.status_code)
 #         print(response.data)
 #
-from django.urls import reverse
-from api.models import Profile
-from django.contrib.auth.models import User
+# from django.urls import reverse
+# from api.models import Profile
+# from django.contrib.auth.models import User
+# from rest_framework.test import APITestCase
+# from rest_framework import status
+#
+# class ProfileTest(APITestCase):
+#
+#     def setUp(self):
+#         # ایجاد یک کاربر مشترک برای همه تست‌ها
+#         self.user = User.objects.create_user(username="testuser", password="1234")
+#         self.client.force_authenticate(user=self.user)
+#
+#     def test_create_profile_authenticated(self):
+#         # تست ایجاد پروفایل با user لاگین‌شده
+#         response = self.client.post(
+#             "/api/profile/",
+#             {
+#                 "name": "Ali",
+#                 "age": 20,
+#                 "email": "alimoh@gmail.com",
+#             },
+#             format="json"
+#         )
+#
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#         print('test_create_profile_authenticated')
+#         print(response.status_code)
+#         print(response.data)
+#
+#     def test_retrieve_inactive_profile(self):
+#         # ایجاد پروفایل inactive با user ست شده
+#         profile = Profile.objects.create(
+#             user=self.user,
+#             name="Reza",
+#             age=40,
+#             email="reza@gmail.com",
+#             isActive=False
+#         )
+#
+#         url = reverse('profile-detail', args=[profile.id])
+#         response = self.client.get(url)
+#
+#         self.assertEqual(response.status_code,403)
+#         self.assertFalse(response.data['success'])
+#         self.assertEqual(
+#             response.data['error']['code'],
+#             'inActive Profile'
+#         )
+#         print('test_retrieve_inactive_profile')
+#         print(response.status_code)
+#         print(response.data)
+#
+#     def test_create_invalid_profile(self):
+#         # تست ارسال داده نامعتبر
+#         url = reverse('profile-list')
+#         data = {"name": "", "age": 10}
+#
+#         response = self.client.post(url, data)
+#
+#         self.assertEqual(response.status_code, 400)
+#         self.assertFalse(response.data['success'])
+#         print("test_create_invalid_profile")
+#         print(response.status_code)
+#         print(response.data)
 from rest_framework.test import APITestCase
 from rest_framework import status
+from django.contrib.auth.models import User
 
-class ProfileTest(APITestCase):
 
-    def setUp(self):
-        # ایجاد یک کاربر مشترک برای همه تست‌ها
-        self.user = User.objects.create_user(username="testuser", password="1234")
-        self.client.force_authenticate(user=self.user)
+class ProfileTests(APITestCase):
 
     def test_create_profile_authenticated(self):
-        # تست ایجاد پروفایل با user لاگین‌شده
-        response = self.client.post(
-            "/api/profile/",
-            {
-                "name": "Ali",
-                "age": 20,
-                "email": "alimoh@gmail.com",
-            },
-            format="json"
-        )
-
+        user = User.objects.create_user(username='john', password='admin')
+        self.client.force_authenticate(user=user)
+        response = self.client.post('/api/profile/',
+                                    {
+                                        'name':'amin',
+                                        'email':'amin@gmail.com',
+                                        'age':20,
+                                        'isActive':False
+                                    })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        print('test_create_profile_authenticated')
         print(response.status_code)
         print(response.data)
-
-    def test_retrieve_inactive_profile(self):
-        # ایجاد پروفایل inactive با user ست شده
-        profile = Profile.objects.create(
-            user=self.user,
-            name="Reza",
-            age=40,
-            email="reza@gmail.com",
-            isActive=False
-        )
-
-        url = reverse('profile-detail', args=[profile.id])
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code,403)
-        self.assertFalse(response.data['success'])
-        self.assertEqual(
-            response.data['error']['code'],
-            'inActive Profile'
-        )
-        print('test_retrieve_inactive_profile')
+        response=self.client.post('/api/profile/',{
+            'name':'amin',
+            'email':'amin',
+            'isActive':15,
+            'age':False
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         print(response.status_code)
         print(response.data)
-
-    def test_create_invalid_profile(self):
-        # تست ارسال داده نامعتبر
-        url = reverse('profile-list')
-        data = {"name": "", "age": 10}
-
-        response = self.client.post(url, data)
-
-        self.assertEqual(response.status_code, 400)
-        self.assertFalse(response.data['success'])
-        print("test_create_invalid_profile")
+        response = self.client.get('/api/profile/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         print(response.status_code)
         print(response.data)
