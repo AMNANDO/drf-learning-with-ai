8434d62c2a86dd0adc9578c1eb6353be72293559
NANDO <amnando.bs@gmail.com>
2025-12-30 13:55:00 +0330

    develop better test file with 5 different test on whole project with setup function and some default records.

--- api/tests.py
+++ api/tests.py
@@ -141,33 +141,54 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth.models import User
+from api.models import Profile
+from django.urls import reverse
 
 
 class ProfileTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username='admin', email='admin@gmail.com', password='admin')
+        cls.profile = Profile.objects.create(name='ali',age=30,email='ali@gmail.com',isActive=True,user=cls.user)
+        cls.profile2 = Profile.objects.create(name='reza',age=17,email='reza@gamil.com',isActive=False,user=cls.user)
 
-    def test_create_profile_authenticated(self):
-        user = User.objects.create_user(username='john', password='admin')
-        self.client.force_authenticate(user=user)
-        response = self.client.post('/api/profile/',
-                                    {
-                                        'name':'amin',
-                                        'email':'amin@gmail.com',
-                                        'age':20,
-                                        'isActive':False
-                                    })
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        print(response.status_code)
-        print(response.data)
-        response=self.client.post('/api/profile/',{
-            'name':'amin',
-            'email':'amin',
-            'isActive':15,
-            'age':False
-        })
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        print(response.status_code)
-        print(response.data)
+    def setUp(self):
+        self.client.force_authenticate(user=self.user)
+    def test_retrieve_list(self):
         response = self.client.get('/api/profile/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.status_code)
-        print(response.data)
+        print('list\n')
+        print(response.status_code,'\n', response.data)
+
+    def test_retrieve_detail(self):
+        response = self.client.get('/api/profile/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print('detail\n')
+        print(response.status_code,'\n', response.data)
+
+    def test_create_profile(self):
+        response = self.client.post('/api/profile/', data={
+            'name':'mohamad',
+            'email':'mohamad@gmail.com',
+            'age':20
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        print('create profile\n')
+        print(response.status_code,'\n', response.data)
+
+    def test_create_invalid_profile(self):
+        response = self.client.post('/api/profile/', data={
+            'name':'',
+            'email':'alireza%game',
+            'age':False,
+            'isActive':25
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        print('create invalid profile\n')
+        print(response.status_code,'\n', response.data)
+
+    def test_retrieve_inActive_profile(self):
+        response = self.client.get('/api/profile/2/')
+        self.assertEqual(response.status_code, 403)
+        print('retrieve inactive profile\n')
+        print(response.status_code,'\n', response.data)
